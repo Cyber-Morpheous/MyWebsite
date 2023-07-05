27d099e44a524b0335eb26c91f77e367998cc316
unknown <richardlongcross00@gmail.com>
2023-07-05 17:23:11 +0500

    added hello animation where the red colour slides out took me a god damn day aik bache ke cartoon rok ke kam kia ha the red colour now slides out and leaves the text behind

--- index.js
+++ index.js
@@ -10,15 +10,3 @@ for (let index = 0; index < HeadingDiv.length; index++) {
 
 var HelloLengthinPercentage = 0.13263
 var dynamic_height = deviceHeight * HelloLengthinPercentage
-var hellos = document.getElementsByClassName("content__container")
-for (let index = 0; index < hellos.length; index++) {
-    hellos[index].style.height = dynamic_height + "px";
-}
-
-var ListElement = document.getElementsByClassName("content__container__list__item")
-
-for (let index = 0; index < ListElement.length; index++) {
-    ListElement[index].style.lineHeight = dynamic_height + "px";
-    ListElement[index].style.fontSize = dynamic_height + "px";
-    ListElement[index].style.fontWeight = (6 * dynamic_height) + "px";
-}
