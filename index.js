19421a8123b017b437758a11fe88a355928e7433
unknown <richardlongcross00@gmail.com>
2023-07-06 15:15:05 +0500

    added the menu its just a div rn, will change its animation and actions later

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var deviceHeight = window.innerHeight;
 console.log(deviceHeight);
 
-var HeadingDiv = document.getElementsByClassName("heading-div");
-console.log(HeadingDiv);
+//var HeadingDiv = document.getElementsByClassName("heading-div");
+//console.log(HeadingDiv);
 
-for (let index = 0; index < HeadingDiv.length; index++) {
-    HeadingDiv[index].style.height = deviceHeight + "px";
-}
+//for (let index = 0; index < HeadingDiv.length; index++) {
+//    HeadingDiv[index].style.height = deviceHeight + "px";
+//}
 
-var HelloLengthinPercentage = 0.13263
-var dynamic_height = deviceHeight * HelloLengthinPercentage
+//var HelloLengthinPercentage = 0.13263
+//var dynamic_height = deviceHeight * HelloLengthinPercentage
